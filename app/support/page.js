16290203e1bf8adb6f799a92c53d100396cba417
dhanushk56 <dhanushsatyapawankarri@gmail.com>
2026-08-08 20:43:47 -0500

    Update support email address in page.js

--- app/support/page.js
+++ app/support/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 // Replace with your actual support server invite
 const SUPPORT_SERVER = "https://support.sparkybot.bond";
-const EMAIL = "support@sparkybot.com";
+const EMAIL = "sparkysupport@gmail.com";
 
 export default function SupportPage() {
   return (
